a5e7f47b9314bf27988692e673ea5a4ba2b75f93
Fenchel <msfenchel@gmail.com>
2012-09-22 18:21:00 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HillbomberRails::Application.config.secret_token = 'c014733234965db41b700c375f0f09dfa18db9c2ab41026208201727762ac0d546e567ec4474815e704bfa86fe9674fe10cc354115b00d519a647c3e4beb0d9b'
