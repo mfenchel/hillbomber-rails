5fbb73037f168ab9dd4d2cdd3da2e6657e6c7a69
Fenchel <msfenchel@gmail.com>
2012-09-22 22:20:00 -0500

    fix appid

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['468347859863144'], ENV['2a09233f9354c1dacac2050c2458feb3']
+  provider :facebook, '468347859863144', '2a09233f9354c1dacac2050c2458feb3'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 HillbomberRails::Application.routes.draw do
   resources :trails
 
+
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
