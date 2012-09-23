962c3de52b1c2c9bfc02415ea7cedbe2f7a92f41
Fenchel <msfenchel@gmail.com>
2012-09-22 23:07:39 -0500

    change session controller to be json

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
+	respond_to :json
+	
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_url
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url
   end
 end
